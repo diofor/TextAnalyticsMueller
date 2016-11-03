f5197a7edbf42d38cc0a7c7be30fdeebe9587139
JMac <JMac@134.91.44.105>
2016-11-03 11:04:14 +0100

    2. Termin -> folie 36

--- de.unidue.langtech.teaching.pp.mueller/src/main/java/de/unidue/langtech/teaching/pp/mueller/pipeline/BasicPipeline.java
+++ de.unidue.langtech.teaching.pp.mueller/src/main/java/de/unidue/langtech/teaching/pp/mueller/pipeline/BasicPipeline.java
@@ -1,9 +1,12 @@
 package de.unidue.langtech.teaching.pp.mueller.pipeline;
 
+import org.apache.uima.fit.component.CasDumpWriter;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 
+import de.tudarmstadt.ukp.dkpro.core.snowball.SnowballStemmer;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.unidue.langtech.teaching.pp.mueller.BaselineExample;
 import de.unidue.langtech.teaching.pp.mueller.EvaluatorExample;
 import de.unidue.langtech.teaching.pp.mueller.ReaderExample;
@@ -20,7 +23,10 @@ public class BasicPipeline
                         ReaderExample.PARAM_INPUT_FILE, "src/test/resources/test/input.txt"
                 ),
                 AnalysisEngineFactory.createEngineDescription(BaselineExample.class),
-                AnalysisEngineFactory.createEngineDescription(EvaluatorExample.class)
+                AnalysisEngineFactory.createEngineDescription(EvaluatorExample.class),
+                AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class),
+                AnalysisEngineFactory.createEngineDescription(SnowballStemmer.class, SnowballStemmer.PARAM_LANGUAGE, "en"),
+                AnalysisEngineFactory.createEngineDescription(CasDumpWriter.class)
         );
     }
 }
